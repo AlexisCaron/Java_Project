46c2ac894e2faf0404f831eb16621fa510100f46
AlexisCaron <alexis.caron@viacesi.fr>
2017-06-20 18:40:21 +0200

    Commit View

---
+++ BoulderDash-BaseProject/BoulderDash/view/src/main/java/view/EventPerformer.java
@@ -0,0 +1,9 @@
+package view;
+
+import java.util.Observer;
+import java.awt.event.KeyListener;
+
+public abstract class EventPerformer extends UpdateSprite implements KeyListener, Observer{
+	public boolean KeyPressed ;
+
+}
---
+++ BoulderDash-BaseProject/BoulderDash/view/src/main/java/view/UpdateSprite.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.JPanel;
+
+public class UpdateSprite extends JPanel{
+	public UpdateSprite(){
+		
+	}
+
+}
