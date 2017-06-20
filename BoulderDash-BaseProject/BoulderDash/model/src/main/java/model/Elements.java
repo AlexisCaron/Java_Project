553219b287c166424c853d7cfd1e187cc202fc9f
AlexisCaron <alexis.caron@viacesi.fr>
2017-06-20 16:21:24 +0200

    Commit Elements

---
+++ BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/Elements.java
@@ -0,0 +1,23 @@
+package model;
+
+public abstract class Elements {
+	private int PositionX;
+	private int PositionY;
+	
+	public void getSprite(){
+		
+	}
+	public int getPositionX() {
+		return PositionX;
+	}
+	public void setPositionX(int positionX) {
+		PositionX = positionX;
+	}
+	public int getPositionY() {
+		return PositionY;
+	}
+	public void setPositionY(int positionY) {
+		PositionY = positionY;
+	}
+
+}
