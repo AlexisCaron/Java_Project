84fc1a501ef5dca25a27e7078440f3e78f59b279
DimitriLibessart <dimitri.libessart@viacesi.fr>
2017-06-23 23:17:20 +0200

    ad javadoc v1.4

--- BoulderDash-BaseProject/BoulderDash/main/src/main/java/main/Pannel.java
+++ BoulderDash-BaseProject/BoulderDash/main/src/main/java/main/Pannel.java
@@ -50,7 +50,7 @@ public class Pannel extends JPanel implements ActionListener{
 
 	/**
 	 * Method for the Diamonds Remaining on the map 
-	 * @return diamondRemaining number of Diamond 
+	 * @return diamondRemaining number of diamond 
 	 */
 	public static int getDiamondRemaining() {
 		return diamondRemaining;
