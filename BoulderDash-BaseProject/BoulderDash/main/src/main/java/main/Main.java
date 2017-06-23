80c9e2337ffb4b9082083990641bb06edd6f854c
flori <flori@10.162.128.179>
2017-06-23 18:20:50 +0200

    V.final

--- BoulderDash-BaseProject/BoulderDash/main/src/main/java/main/Main.java
+++ BoulderDash-BaseProject/BoulderDash/main/src/main/java/main/Main.java
@@ -1,64 +1,22 @@
 package main;
 
-import java.sql.SQLException;
-
 import javax.swing.*;
 
 
-import controller.ControllerFacade;
-import model.ModelFacade;
-import view.ViewFacade;
 
-/**
- * <h1>The Class Main.</h1>
- *
- * @author Jean-Aymeric DIET jadiet@cesi.fr
- * @version 1.0
- */
 public class Main {
-
-	
-    /**
-     * The main method.
-     *
-     * @param args
-     *            the arguments
-     */
-    public static void main(final String[] args) {
-   
-    		new Main();
-    		
-    
-    		
-    		
-
-    
-
-        final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
-
-        try {
-            controller.start();
-        } catch (final SQLException exception) {
-            exception.printStackTrace();
-        }
-    }
-
-
-
-public Main(){
-	JFrame fenetre = new  JFrame("Jeu");
-	//*fenetre.add(new Panneau());
-	fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	fenetre.setSize(1088, 864);
-	fenetre.setLocationRelativeTo(null);
-	fenetre.setResizable(false);
-	fenetre.setVisible(true);
-	
+	public static void main(String[] args) {
+		new Main();
+		
 	}
+		public Main(){
+		JFrame screen = new  JFrame("BoulderDash");
+		screen.add(new Pannel());
+		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		screen.setSize(1080, 1080);
+		screen.setLocationRelativeTo(null);
+		screen.setResizable(false);
+		screen.setVisible(true);
+	}
+
 }
-
-
-
-
-
-
